d21935291d17dbafb8886ac2729dbe812be7ae1b
SuperGena <39839502+SuperGena@users.noreply.github.com>
2021-05-18 12:58:49 +0300

    Started weapon factory for injection

---
+++ Zenject/Assets/Scripts/Factories/Rifle.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rifle
+{
+    private float _damage;
+    private float _reloadSpeed;
+    private float _bulletSpeed;
+    private Projectile _projectile;
+}
